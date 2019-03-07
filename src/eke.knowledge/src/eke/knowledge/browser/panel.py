5249aa6d928ec48345da91c57731b33a15131d38
Sean Kelly <kelly@seankelly.biz>
2019-03-06 20:45:42 -0600

    Add reset action to control panel; add control panel

--- src/eke.knowledge/src/eke/knowledge/browser/panel.py
+++ src/eke.knowledge/src/eke/knowledge/browser/panel.py
@@ -7,6 +7,7 @@ from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from eke.knowledge import _
 from eke.knowledge.interfaces import IPanel
+from eke.knowledge.rdfingestor import DAWN_OF_TIME
 
 
 class PanelEditForm(RegistryEditForm):
@@ -14,6 +15,11 @@ class PanelEditForm(RegistryEditForm):
     schema = IPanel
     label = _(u'Knowledge Controls')
     description = _(u'Control panel for EDRN Knowledge Environment')
+    def applyChanges(self, data):
+        resetIngestState = data.get('resetIngestState', False)
+        if resetIngestState:
+            data['ingestEnabled'], data['resetIngestState'], data['ingestStart'] = True, False, DAWN_OF_TIME
+        super(PanelEditForm, self).applyChanges(data)
 
 
 class KnowledgeControlPanel(ControlPanelFormWrapper):
