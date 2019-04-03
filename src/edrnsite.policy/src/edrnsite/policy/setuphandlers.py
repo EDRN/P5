ec527705c77bc24e9ea7055904d278d8f9dd0d64
Sean Kelly <kelly@seankelly.biz>
2019-04-03 07:35:25 -0500

    Make the home page image smaller

--- src/edrnsite.policy/src/edrnsite/policy/setuphandlers.py
+++ src/edrnsite.policy/src/edrnsite/policy/setuphandlers.py
@@ -25,9 +25,10 @@ _ITEMS_TO_DELETE = ('news', 'events', 'Members')
 _HOME_PAGE_DESC = u'''The Early Detection Research Network (EDRN), an initiative of the National Cancer Institute (NCI), brings together dozens of institutions to help accelerate the translation of biomarker information into clinical applications and to evaluate new ways of testing cancer in its earliest stages and for cancer risk.'''
 _HOME_PAGE_BODY = u'''<h2>Getting started…</h2><p>
 <img
-    src='resolveuid/{collage}'
+    src='resolveuid/{collage}/@@images/image/preview'
     class='image-right'
     data-linktype='image'
+    data-scale='preview'
     data-val='{collage}'
 />
 <strong>Check out the <a class="internal-link" href="resources/highlights">EDRN Highlights</a> — a listing of our accomplishments and milestones.</strong>
