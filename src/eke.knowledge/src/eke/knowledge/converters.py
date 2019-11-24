1e10f64dde569dbc642d4785815f533436678246
Sean Kelly <kelly@seankelly.biz>
2019-11-24 16:17:39 -0600

    Adapters for search-indexing on relation fields

---
+++ src/eke.knowledge/src/eke/knowledge/converters.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+
+from collective.dexteritytextindexer.interfaces import IDexterityTextIndexFieldConverter
+from collective.dexteritytextindexer.converters import DefaultDexterityTextIndexFieldConverter
+from plone.dexterity.interfaces import IDexterityContent
+from z3c.form.interfaces import IWidget
+from z3c.relationfield.interfaces import IRelationChoice, IRelationList
+from zope.component import adapter
+from zope.interface import implementer
+
+# TODO: If this works, we can try it for mutli-valued relations too:
+# from z3c.relationfield.interfaces import IRelationList  #
+
+
+@implementer(IDexterityTextIndexFieldConverter)
+@adapter(IDexterityContent, IRelationChoice, IWidget)
+class RelationChoiceFieldConverter(DefaultDexterityTextIndexFieldConverter):
+    u'''Make relation 1–1 fields searchable by de-referencing their objects and using their
+    titles their search corpus.  Maybe the Plone Collectiive would find this useful?
+    '''
+    def convert(self):
+        storage = self.field.interface(self.context)
+        rv = self.field.get(storage)
+        if rv and rv.to_object and rv.to_object.title:
+            return rv.to_object.title.encode('utf-8')
+        return ''
+
+
+@implementer(IDexterityTextIndexFieldConverter)
+@adapter(IDexterityContent, IRelationList, IWidget)
+class RelationListFieldConverter(DefaultDexterityTextIndexFieldConverter):
+    u'''Make relation 1–n fields searchable by de-referencing their objects and using
+    their titles as the search corpus. Maybe the Plone Collective would find this useful?
+    '''
+    def convert(self):
+        storage = self.field.interface(self.context)
+        rvs = self.field.get(storage)
+        titles = []
+        if rvs:
+            for rv in rvs:
+                if rv.to_object and rv.to_object.title:
+                    titles.append(rv.to_object.title.encode('utf-8'))
+        return ' '.join(titles)
