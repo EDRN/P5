ec4ddb8c260a102e9d55681c807fee36d92f4a53
Sean Kelly <kelly@seankelly.biz>
2019-12-24 16:11:30 -0600

    Show some more diagnostics

--- .github/actions/buildout/entrypoint.sh
+++ .github/actions/buildout/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -l
 #
 
-echo "===== OK we are in ====="
-echo "===== args = $@ "
-echo "==== env follows"
+echo "🚗 ===== OK we are in ====="
+echo "⚙️ ===== args = $@ "
+echo "🌳 ===== env follows"
 env
+echo "🌳 ===== end env "
+echo "📁 ===== directory / follows"
+ls -F /
+echo "📁 ===== directory / ends"
+echo "📂 ===== workspace follows"
+ls -F /github/workspace
+echo "📂 ===== workspace ends"
+echo "🛑 the end"
 exit 0
