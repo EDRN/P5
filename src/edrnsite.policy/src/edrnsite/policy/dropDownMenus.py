5633c9b9abc7c9c07643ebf1ae9b8ea8a3a440f3
Sean Kelly <kelly@seankelly.biz>
2021-01-11 14:23:18 -0600

    Re-enable

--- src/edrnsite.policy/src/edrnsite/policy/dropDownMenus.py
+++ src/edrnsite.policy/src/edrnsite/policy/dropDownMenus.py
@@ -38,22 +38,22 @@ def installDataAndResources(context):
 
     # 👉 Okay, big moves:
 
-    # pac.move(source=pac.get('/biomarkers'), target=dataAndResources)
-    # pac.move(source=pac.get('/protocols'), target=dataAndResources)
-    # pac.move(source=pac.get('/data'), target=dataAndResources)
-    # pac.move(source=pac.get('/publications'), target=dataAndResources)
-    # pac.move(source=pac.get('/informatics'), target=dataAndResources)
-    # pac.move(source=pac.get('/resources/sample-reference-sets'), target=dataAndResources)
+    pac.move(source=pac.get('/biomarkers'), target=dataAndResources)
+    pac.move(source=pac.get('/protocols'), target=dataAndResources)
+    pac.move(source=pac.get('/data'), target=dataAndResources)
+    pac.move(source=pac.get('/publications'), target=dataAndResources)
+    pac.move(source=pac.get('/informatics'), target=dataAndResources)
+    pac.move(source=pac.get('/resources/sample-reference-sets'), target=dataAndResources)
 
     # During development, I'm just doing this instead of the above:
-    pac.delete(obj=pac.get('/biomarkers'), check_linkintegrity=False)
-    # pac.delete(obj=pac.get('/protocols'), check_linkintegrity=False)  # Can't delete this; causes error
-    # So for now, delete its contents and move its container
-    for item in pac.get('/protocols').values():
-        pac.delete(obj=item, check_linkintegrity=False)
-    pac.move(source=pac.get('/protocols'), target=dataAndResources)
-    pac.delete(obj=pac.get('/data'), check_linkintegrity=False)
-    pac.delete(obj=pac.get('/publications'), check_linkintegrity=False)
+    # pac.delete(obj=pac.get('/biomarkers'), check_linkintegrity=False)
+    # # pac.delete(obj=pac.get('/protocols'), check_linkintegrity=False)  # Can't delete this; causes error
+    # # So for now, delete its contents and move its container
+    # for item in pac.get('/protocols').values():
+    #     pac.delete(obj=item, check_linkintegrity=False)
+    # pac.move(source=pac.get('/protocols'), target=dataAndResources)
+    # pac.delete(obj=pac.get('/data'), check_linkintegrity=False)
+    # pac.delete(obj=pac.get('/publications'), check_linkintegrity=False)
     # # pac.delete(obj=pac.get('/resources/sample-reference-sets'), check_linkintegrity=False)  # Can't delete this either; error
 
     # HK wants LabCAS in a sub menu
