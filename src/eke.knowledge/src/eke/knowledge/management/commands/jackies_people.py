b8de0303ed1ac39d6fe392aa366fd1d28e13feed
Sean Kelly <kelly@seankelly.biz>
2022-10-20 14:42:46 -0500

    Jackie's request: parameterize on a single site

--- src/eke.knowledge/src/eke/knowledge/management/commands/jackies_people.py
+++ src/eke.knowledge/src/eke/knowledge/management/commands/jackies_people.py
@@ -4,7 +4,7 @@
 
 
 from django.core.management.base import BaseCommand
-from eke.knowledge.models import Person
+from eke.knowledge.models import Person, Site
 from django.db.models.functions import Lower
 import csv, argparse, sys
 
@@ -17,9 +17,13 @@ class Command(BaseCommand):
             '--output-csv', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
             help='Where to write the CSV file, defaults to stdout'
         )
+        parser.add_argument('--site-id', type=int, help='Limit report to one site ID')
 
     def handle(self, *args, **options):
-        people = Person.objects.all().order_by(Lower('title'))
+        if options['site_id']:
+            people = Site.objects.filter(dmccSiteID=options['site_id']).first().get_children().order_by(Lower('title')).specific()
+        else:
+            people = Person.objects.all().order_by(Lower('title'))
         writer = csv.writer(options['output_csv'])
         for person in people:
             writer.writerow((person.personID, person.title))
