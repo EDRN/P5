78570151162f42370775766f8da7419fdb340993
Sean Kelly <kelly@seankelly.biz>
2022-11-30 14:53:10 -0600

    Production support now that we are fully on wagtail

---
+++ src/edrnsite.policy/src/edrnsite/policy/management/commands/edrndevreset.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+'''🧬 EDRN Site: development reset.'''
+
+from django.core.management.base import BaseCommand
+from edrnsite.controls.models import Informatics
+from wagtail.models import Site
+from robots.models import Rule, DisallowedUrl
+
+
+class Command(BaseCommand):
+    '''The EDRN development reset command.'''
+
+    help = 'Reset the EDRN P5 portal for use in development'
+
+    def handle(self, *args, **options):
+        '''Handle the EDRN `edrndevreset` command.'''
+        site = Site.objects.filter(is_default_site=True).first()
+        assert site.site_name == 'Early Detection Research Network'
+
+        informatics = Informatics.for_site(site)
+        informatics.in_development = True
+        informatics.save()
+
+        Rule.objects.filter(sites=site).delete()
+        rule = Rule(robot='*')
+        rule.save()
+        rule.sites.add(site)
+        url = DisallowedUrl(pattern='/')
+        rule.disallowed.add(url)
+        url.save()
+        rule.save()
---
+++ support/sync-from-ops.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+# Sync NCI Operations
+# ===================
+#
+# This syncs the P5 database from NCI operations to the local directory.
+#
+# It expects the NIH_PASSWORD environment variable set to the password allowed
+# access to the NCI operations database over HTTP.
+#
+# Set Up
+# ------
+#
+# Get our environment and check things before we go.
+
+# Inherit settings from environment
+NIH_USERNAME=${NIH_USERNAME:-kellysc}
+NIH_PASSWORD=${NIH_PASSWORD:-}
+WORKSPACE=${WORKSPACE:-${PWD:-`pwd`}}
+
+# Check the password
+if [ \! -n "$NIH_PASSWORD" ]; then
+    echo "❌ The NIH_PASSWORD environment must be set" 1>&2
+    exit 1
+fi
+
+# Here We Go
+# ----------
+
+echo "ℹ️ Syncing with username «${NIH_USERNAME}» to «${WORKSPACE}» in local directory" 1>&2
+
+media=${WORKSPACE}/media
+source=https://edrn.nci.nih.gov/database-access
+
+
+# PostgreSQL Database
+# -------------------
+#
+# We always assume this thing is changing and frequently, so we delete our
+# local copy and get a fresh one every time.
+
+echo "📈 Retrieving database" 1>&2
+rm -f "edrn.sql.bz2"
+wget \
+    --quiet \
+    --execute robots=off \
+    --timestamping \
+    --no-check-certificate \
+    --user="$NIH_USERNAME" \
+    --password="$NIH_PASSWORD" \
+    "$source/edrn.sql.bz2"
+
+
+# Media Blobs
+# -----------
+#
+# There are 7+ gigabytes worth of blobs and they hardly change, so we
+# definitely take advantage of the timestamping and mirroring features of
+# ``wget`` in order to speed things up on subsequent runs.
+
+echo "📀 Retrieving blobs" 1>&2
+[ -d "$media" ] || mkdir -p "$media"
+for media_type in documents images original_images; do
+    [ -d "$media/$media_type" ] || mkdir -p "$media/$media_type"
+    cd "$media/$media_type"
+    wget \
+        --quiet \
+        --execute robots=off \
+        --cut-dirs=2 \
+        --reject='index.html*' \
+        --no-host-directories \
+        --mirror \
+        --no-parent \
+        --relative \
+        --timestamping \
+        --no-check-certificate \
+        --recursive \
+        --user="$NIH_USERNAME" \
+        --password="$NIH_PASSWORD" \
+        "$source/$media_type/"
+done
+
+echo "😌 All done" 1>&2
+exit 0
