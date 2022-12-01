677beea3409b64c84124a826d4127b742a0ecfda
Sean Kelly <kelly@seankelly.biz>
2022-12-01 10:31:54 -0600

    Resolve #166

--- src/edrn.auth/src/edrn/auth/urls.py
+++ src/edrn.auth/src/edrn/auth/urls.py
@@ -3,9 +3,10 @@
 '''🔐 EDRN auth: URL patterns.'''
 
 from django.urls import path
-from .views import LogoutView
+from .views import LogoutView, authentication_test_view
 
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('authentication-test', authentication_test_view, name='authentication-test'),
 ]
--- src/edrn.auth/src/edrn/auth/views.py
+++ src/edrn.auth/src/edrn/auth/views.py
@@ -6,6 +6,53 @@ from django.contrib.auth import views as auth_views
 from django.http import HttpRequest, HttpResponse
 from django.contrib import messages
 from django.conf import settings
+from django.contrib.auth import authenticate, login
+import base64, http
+
+
+def view_or_basicauth(view, request: HttpRequest, test_func, realm: str = '', *args, **kwargs):
+    '''Check if a user is logged in or not or if there's HTTP basic authentication.
+
+    Adapted from https://github.com/m7v8/django-basic-authentication-decorator
+    '''
+    if test_func(request.user):
+        return view(request, *args, **kwargs)
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                uname, passwd = base64.b64decode(auth[1]).decode('utf-8').split(':', 1)
+                user = authenticate(username=uname, password=passwd)
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        request.user = user
+                        if test_func(request.user):
+                            return view(request, *args, **kwargs)
+    response = HttpResponse()
+    response.status_code = http.HTTPStatus.UNAUTHORIZED
+    response['WWW-Authenticate'] = f'Basic realm="{realm}"'
+    return response
+
+
+def logged_in_or_basicauth(realm: str = ''):
+    '''Decorate a view so that it requires a logged-in user or HTTP basic auth to be present.''
+
+    Adapated from https://github.com/m7v8/django-basic-authentication-decorator
+    '''
+    def view_decorator(func):
+        def wrapper(request, *args, **kwargs):
+            return view_or_basicauth(func, request, lambda u: u.is_authenticated, realm, *args, **kwargs)
+        return wrapper
+    return view_decorator
+
+
+@logged_in_or_basicauth('edrn')
+def authentication_test_view(request: HttpRequest) -> HttpResponse:
+    '''A basic view that just simply requires you to be logged in and returns an empty payload.'''
+    response = HttpResponse('')
+    response.status_code = http.HTTPStatus.OK
+    return response
 
 
 class LogoutView(auth_views.LogoutView):
