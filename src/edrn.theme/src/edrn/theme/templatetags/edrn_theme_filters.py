1f4856af1ee1d0e688912260375e749d29ab22c0
Sean Kelly <kelly@seankelly.biz>
2023-01-13 14:24:56 -0600

    Resolve #207

--- src/edrn.theme/src/edrn/theme/templatetags/edrn_theme_filters.py
+++ src/edrn.theme/src/edrn/theme/templatetags/edrn_theme_filters.py
@@ -10,6 +10,11 @@ register = template.Library()
 
 @register.filter(is_safe=False)
 def edrn_sorted_menu(menu_items):
-    '''Sort menu items by their titles.'''
+    '''Sort menu items by their titles.
+
+    Note that after years of having alphabetical menus, Christos now wants the drop-downs to be
+    in "significance" order (#207). So this function is no longer being called from the
+    ``sub_menu_dropdown.html`` template. It's preserved here in case he changes his mind again.
+    '''
     menu_items.sort(key=lambda item: item.title)
     return menu_items
