3ab9520db8285828a0a740ca351f60ee291e8b6f
Sean Kelly <kelly@seankelly.biz>
2023-10-12 10:06:00 -0500

    Fix #232 by adding basic auth support and redir to /

--- src/edrnsite.policy/src/edrnsite/policy/views.py
+++ src/edrnsite.policy/src/edrnsite/policy/views.py
@@ -4,13 +4,22 @@
 
 from django.core.cache import caches
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from edrn.auth.views import logged_in_or_basicauth
 
 
+def _get_referrer(request: HttpRequest) -> str:
+    try:
+        return request.META['HTTP_REFERER']
+    except KeyError:
+        return '/'
+
+
+@logged_in_or_basicauth('edrn')
 def clear_caches(request: HttpRequest) -> HttpResponse:
     '''Clear all caches.'''
     if request.user.is_staff or request.user.is_superuser:
         for cache in caches:
             caches[cache].clear()
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        return HttpResponseRedirect(_get_referrer(request))
     else:
         return HttpResponseForbidden()
