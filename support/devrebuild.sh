c81604194df6e00aa70a78fe7d1c229598f84acc
Sean Kelly <kelly@seankelly.biz>
2023-11-29 13:43:13 -0600

    Support for #309

--- support/devrebuild.sh
+++ support/devrebuild.sh
@@ -58,8 +58,8 @@ bzip2 --decompress --stdout edrn.sql.bz2 | psql --dbname=edrn --echo-errors --qu
 ./manage.sh edrndevreset
 
 # Add additional upgrade steps here:
-# ./manage.sh edrn_nukephotos  # 6.7
-# ./manage.sh install_member_finder_page  # 6.7
+#
+# Currently for 6.8 there are no needed upgrade steps
 
 # This may be optional if you want to save time:
 ./manage.sh rdfingest
