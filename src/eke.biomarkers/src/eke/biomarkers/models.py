275d8b08d3626a8d17c089f7e5fac55aab169296
Sean Kelly <kelly@seankelly.biz>
2025-08-15 09:28:41 -0500

    Make the BiomarkerBodySystem available as a facing model

--- src/eke.biomarkers/src/eke/biomarkers/models.py
+++ src/eke.biomarkers/src/eke/biomarkers/models.py
@@ -2,7 +2,7 @@
 
 '''🧫 EDRN Knowledge Environment Biomarkers: models.'''
 
-from .biomarker import Biomarker
+from .biomarker import Biomarker, BiomarkerBodySystem
 from .biomarkerindex import BiomarkerIndex
 
-__all__ = [Biomarker, BiomarkerIndex]
+__all__ = [Biomarker, BiomarkerBodySystem, BiomarkerIndex]
